5715d46592c881893c1c400c969d79e923fbff55
a.tsebenko <a.tsebenko@trueengineering.ru>
2025-03-11 08:16:07 +0700

    - добавлены зависимости скоупа test. Тесты перенесены

--- server/src/test/java/ru/jafti/braintalk/server/controller/SendControllerTest.java
+++ server/src/test/java/ru/jafti/braintalk/server/controller/SendControllerTest.java
@@ -1,9 +1,9 @@
 package ru.jafti.braintalk.server.controller;
 
+import org.junit.jupiter.api.Test;
 import ru.jafti.braintalk.server.RendezvousPoint;
 import ru.jafti.braintalk.server.connection.Channel;
 import ru.jafti.braintalk.server.connection.Session;
-import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
