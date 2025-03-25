ade5d9938132250b0b4894542bddb285ab6c22de
a.tsebenko <a.tsebenko@trueengineering.ru>
2025-03-25 10:23:15 +0700

    - Добавляет Работу с БД
      А так же: меняем плагин сборки jar на maven-shade-plugin
      для того чтобы собирать fat jar.
      В предыдущем плагине (maven-jar-plugin) для подкладывания внешних библиотек нужно больше усилий.
      Ещё одно достоинство shade плагина, он создаёт после сборки артефакт dependency-reduced-pom.xml в нём можно посмотреть результирующий граф зависимостей. Добавил его в gitignore

---
+++ server/src/main/java/ru/jafti/braintalk/server/persist/DbConnection.java
@@ -0,0 +1,23 @@
+package ru.jafti.braintalk.server.persist;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    public void connect() {
+        String btPostgresUser = System.getenv("BT_POSTGRES_USER");
+        String btPostgresPass = System.getenv("BT_POSTGRES_PASS");
+        String jdbcUrl = System.getenv("JDBC_URL");
+
+        // Создание соединения
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, btPostgresUser, btPostgresPass)) {
+            if (connection != null) {
+                System.out.println("Соединение с PostgreSQL установлено!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения к базе данных: " + e.getMessage());
+        }
+    }
+}
