8b7fbdc24451bbefb66068bc5a6041a4af420176
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-04-03 14:30:24 +0700

    Добавил реализации методов TalkerProfileServiceImpl.findByNickname() и TalkerProfileServiceImpl.createWithNickname()

--- server/src/main/java/ru/jafti/braintalk/server/TalkerProfileServiceImpl.java
+++ server/src/main/java/ru/jafti/braintalk/server/TalkerProfileServiceImpl.java
@@ -2,19 +2,56 @@ package ru.jafti.braintalk.server;
 
 import ru.jafti.braintalk.server.persist.DbConnection;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
 import java.util.UUID;
 
 public class TalkerProfileServiceImpl implements TalkerProfileService {
+    private final Statement dbStatement;
+
     public TalkerProfileServiceImpl(DbConnection dbConnection) {
+        try {
+            dbStatement = dbConnection.getConnection().createStatement();
+
+            var createTableSql = "CREATE TABLE IF NOT EXISTS talker_profile (talker_id UUID PRIMARY KEY, nickname TEXT)";
+            dbStatement.execute(createTableSql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public UUID findByNickname(String nickname) {
-        return null;
+        try {
+            var selectSql = "SELECT * FROM talker_profile";
+            var resultSet = dbStatement.executeQuery(selectSql);
+
+            while (resultSet.next()) {
+                var uuid = UUID.fromString(resultSet.getString("talker_id"));
+                var storedNickname = resultSet.getString("nickname");
+
+                if (Objects.equals(nickname, storedNickname)) {
+                    return uuid;
+                }
+            }
+
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public UUID createWithNickname(String nickname) {
-        return null;
+        var uuid = UUID.randomUUID();
+        var insertSql = String.format("INSERT INTO talker_profile VALUES ('%s', '%s')", uuid, nickname);
+
+        try {
+            dbStatement.executeUpdate(insertSql);
+            return uuid;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
