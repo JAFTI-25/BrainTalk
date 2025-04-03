9c18251a9258c02187aeb14bdf74c35d7c94834a
aaa28 <aaa28@DRAGON-ARMY>
2025-04-03 17:24:20 +0700

    #22 реализует автоматический логин пользователя.

--- server/src/main/java/ru/jafti/braintalk/server/service/TalkerProfileService.java
+++ server/src/main/java/ru/jafti/braintalk/server/service/TalkerProfileService.java
@@ -1,8 +1,10 @@
-package ru.jafti.braintalk.server;
+package ru.jafti.braintalk.server.service;
 
 import java.util.UUID;
 
 public interface TalkerProfileService {
+    /// if not found returns null
+    String findById(UUID id);
     UUID findByNickname(String nickname);
     UUID createWithNickname(String nickname);
 }
