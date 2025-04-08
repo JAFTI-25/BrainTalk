9a006e0e04ef313a67b4e5c0dee3df6657db4dad
a.tsebenko <a.tsebenko@trueengineering.ru>
2025-04-08 22:50:44 +0700

    #15 Добавил реализацию клиента
      так же поменял порт SSL на 9443 чтобы можно было разделять в будущем открытый сокет и SSL

--- server/src/main/java/ru/jafti/braintalk/server/BrainTalkServerApplication.java
+++ server/src/main/java/ru/jafti/braintalk/server/BrainTalkServerApplication.java
@@ -2,32 +2,30 @@ package ru.jafti.braintalk.server;
 
 import ru.jafti.braintalk.server.connection.ConnectionHandler;
 import ru.jafti.braintalk.server.persist.DbConnection;
+import ru.jafti.braintalk.server.socket.SSLSocketFactory;
+import ru.jafti.braintalk.server.socket.SocketFactory;
 
-import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class BrainTalkServerApplication {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         new BrainTalkServerApplication().start();
     }
 
     public void start() {
         new DbConnection().connect();
+        try {
+            SocketFactory socketFactory = new SSLSocketFactory();
+            try (var serverSocket = socketFactory.getSocket(9000)) {
+                System.out.println("Server is listening on port 9000");
 
-        try (var serverSocket = new ServerSocket(9000)) {
-            System.out.println("Server is listening connections on port " + 9000);
-
-            while (true) {
-                Socket accept = serverSocket.accept();
-                new ConnectionHandler(accept).start();
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    new ConnectionHandler(socket).start();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
     }
 }
-
-
-
