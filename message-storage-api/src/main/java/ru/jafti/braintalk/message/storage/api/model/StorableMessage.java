7c78835667e3cd920d9e27174cac1f80e9cb2e53
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-04-24 14:40:16 +0700

    refs #41 Добавили модуль message-storage-api и StorableMessage.

---
+++ message-storage-api/src/main/java/ru/jafti/braintalk/message/storage/api/model/StorableMessage.java
@@ -0,0 +1,37 @@
+package ru.jafti.braintalk.message.storage.api.model;
+
+import java.util.UUID;
+
+public record StorableMessage(
+        String messageId,
+        From from,
+        To to,
+        Content content
+) {
+
+    public record From(String nickname, UUID talkerGuid) {
+    }
+
+    public record To(String nickname) {
+    }
+
+    public record Content(String rawContent, Content.ContentType contentType) {
+
+        public enum ContentType {
+            TEXT
+        }
+    }
+
+    public static StorableMessage buildFrom(
+            String messageId,
+            String fromTalkerNickName,
+            UUID fromTalkerGuid,
+            String toTalkerNickname,
+            String message) {
+        var from = new From(fromTalkerNickName, fromTalkerGuid);
+        var to = new StorableMessage.To(toTalkerNickname);
+        var content = new Content(message, Content.ContentType.TEXT);
+
+        return new StorableMessage(messageId, from, to, content);
+    }
+}
