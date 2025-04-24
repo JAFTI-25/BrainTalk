ff3f71b8240895ab7178cc0ea5b08d6d70c6312d
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-04-24 16:12:11 +0700

    refs #41 Добавил модуль common с константами

---
+++ common/src/main/java/ru/jafti/braintalk/common/CommonConstants.java
@@ -0,0 +1,5 @@
+package ru.jafti.braintalk.common;
+
+public class CommonConstants {
+    public static final String SYSTEM_TALKER = "BrainTalk";
+}
--- server/src/main/java/ru/jafti/braintalk/server/Constants.java
+++ server/src/main/java/ru/jafti/braintalk/server/Constants.java
@@ -1,7 +1,6 @@
 package ru.jafti.braintalk.server;
 
 public class Constants {
-    public static final String SYSTEM_TALKER = "BrainTalk";
     public static final String LOGIN = "/login";
     public static final String AUTO_LOGIN = "/auto-login";
     public static final String REGISTER = "/register";
