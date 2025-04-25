f130e08aece422be0f1bde3403cbf3cf42ef85ed
a.tsebenko <a.tsebenko@trueengineering.ru>
2025-04-25 08:26:19 +0700

    refs #43 Мои правки

--- server/src/main/java/ru/jafti/braintalk/server/snowflake/MessageIdGeneratorImpl.java
+++ server/src/main/java/ru/jafti/braintalk/server/snowflake/MessageIdGeneratorImpl.java
@@ -1,14 +1,22 @@
 package ru.jafti.braintalk.server.snowflake;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import xyz.downgoon.snowflake.Snowflake;
 
 @Component
 public class MessageIdGeneratorImpl implements MessageIdGenerator {
 
-    static Snowflake snowflake = new Snowflake(1, 2);
+    private final Snowflake snowflake;
 
+    public MessageIdGeneratorImpl(
+            @Value("${snowflake.datacenterId:1}") Long datacenterId,
+            @Value("${snowflake.workerId:2}") Long workerId
+    ) {
+        this.snowflake = new Snowflake(datacenterId, workerId);
+    }
 
-    public long generate(){
+    public long generate() {
         return snowflake.nextId();
     }
 
