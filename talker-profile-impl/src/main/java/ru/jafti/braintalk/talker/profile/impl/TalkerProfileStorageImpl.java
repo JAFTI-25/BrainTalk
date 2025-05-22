e60ed2ffb6bc7f7811c69f08d3aafbeedcc6c915
p-borozdin <p.borozdin.bsnss@gmail.com>
2025-05-22 13:16:00 +0700

    Убрал ненужный import

--- talker-profile-impl/src/main/java/ru/jafti/braintalk/talker/profile/impl/TalkerProfileStorageImpl.java
+++ talker-profile-impl/src/main/java/ru/jafti/braintalk/talker/profile/impl/TalkerProfileStorageImpl.java
@@ -1,6 +1,5 @@
 package ru.jafti.braintalk.talker.profile.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.jafti.braintalk.talker.profile.api.TalkerProfileStorage;
 import ru.jafti.braintalk.talker.profile.impl.repository.TalkerProfileRepository;
